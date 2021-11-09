18443f07e4bf63c23f0c3fa1c6b43658963ebb04
KobeanH <90979360+KobeanH@users.noreply.github.com>
2021-11-10 00:43:46 +0900

    customhooks2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./styles.css";
 import { UserCard } from "./components/UserCard";
 import axios from "axios";
 import { User } from "./types/api/user";
+import { useState } from "react";
+import { UserProfile } from "./types/userProfile";
 
 const user = {
   id: 1,
@@ -11,13 +13,28 @@ const user = {
 };
 
 export default function App() {
+  const { userProfiles, setUserProfiles } = useState<Array<UserProfile>>({});
+
   const onClickFetchUser = () => {
-    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users");
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      });
   };
+
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
-      <UserCard user={user} />
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 }
